2e7691e74394b4ea9c7e5608520e81f502769e6f
chaim1 <cohen1ch@gmail.com>
2019-01-20 23:31:10 +0200

    update

--- Project-Course/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Project-Course/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,8 +9,8 @@ import { Recipe } from '../recipes.model';
 export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
-    new Recipe( 'A Test Recipe', 'This test recipe', 'http://4k.com/wp-content/uploads/2014/06/4k-image-tiger-jumping.jpg'),
-    new Recipe( 'A Test Recipe', 'This test recipe', 'http://4k.com/wp-content/uploads/2014/06/4k-image-tiger-jumping.jpg')
+    new Recipe( 'A Test Recipe 1', 'This test recipe', 'http://4k.com/wp-content/uploads/2014/06/4k-image-tiger-jumping.jpg'),
+    new Recipe( 'A Test Recipe 2', 'This test recipe', 'http://i.imgur.com/RRUe0Mo.png')
   ];
   constructor() { }
 
