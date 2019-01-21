7658d6b2b5b14b41b0c40739d38c3f9cd6ead092
chaim1 <cohen1ch@gmail.com>
2019-01-21 13:24:26 +0200

    update

--- directives-start/src/app/better-highlight/better-highlight.directive.ts
+++ directives-start/src/app/better-highlight/better-highlight.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, OnInit, Renderer2, ElementRef, HostListener, HostBinding } from '@angular/core';
+import { Directive, OnInit, Renderer2, ElementRef, HostListener, HostBinding, Input } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
@@ -6,16 +6,19 @@ import { Directive, OnInit, Renderer2, ElementRef, HostListener, HostBinding } f
 export class BetterHighlightDirective implements OnInit{
 
   constructor(private elRef: ElementRef, private rendera: Renderer2) { }
-@HostBinding('style.backgroundColor') backgroundColor: string = 'yellow';
+  @Input() deafultColor: string = 'yellow';
+  @Input() highlightColor: string = 'blue';
+  @HostBinding('style.backgroundColor') backgroundColor: string = 'yellow';
   ngOnInit(){
+    this.backgroundColor = this.deafultColor;
     // this.rendera.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
   }
-@HostListener('mouseenter') mouseover(eventData: Event){
+  @HostListener('mouseenter') mouseover(eventData: Event){
   // this.rendera.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
-  this.backgroundColor = 'blue';
+  this.backgroundColor = this.highlightColor;
 }
-@HostListener('mouseleave') mouseleave(eventData: Event){
+  @HostListener('mouseleave') mouseleave(eventData: Event){
   // this.rendera.setStyle(this.elRef.nativeElement, 'background-color', 'yellow');
-  this.backgroundColor = 'green';
+  this.backgroundColor = this.deafultColor;
 }
 }
