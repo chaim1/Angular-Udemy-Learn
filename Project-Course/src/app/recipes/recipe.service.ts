92b2739e44d63dd50ad86413ddf8345fe957af11
chaim1 <cohen1ch@gmail.com>
2019-02-03 16:13:13 +0200

    update

--- Project-Course/src/app/recipes/recipe.service.ts
+++ Project-Course/src/app/recipes/recipe.service.ts
@@ -1,12 +1,25 @@
 import { EventEmitter } from '@angular/core';
 
 import { Recipe } from './recipes.model';
+import { Ingredient } from '../shared/ingredien.model';
 
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
     private recipes: Recipe[] = [
-        new Recipe( 'A Test Recipe 1', 'This test recipe', 'http://4k.com/wp-content/uploads/2014/06/4k-image-tiger-jumping.jpg'),
-        new Recipe( 'A Test Recipe 2', 'This test recipe', 'http://i.imgur.com/RRUe0Mo.png')
+        new Recipe( 'A Test Recipe 1',
+         'This test recipe', 
+         'http://4k.com/wp-content/uploads/2014/06/4k-image-tiger-jumping.jpg',
+         [
+             new Ingredient('Meat', 6),
+             new Ingredient('France Fries',20)
+         ]),
+        new Recipe( 'A Test Recipe 2', 
+        'This test recipe', 
+        'http://i.imgur.com/RRUe0Mo.png',
+        [
+            new Ingredient('Bunz', 4),
+            new Ingredient('Meat',1)
+        ])
       ];
 
       getRecipes(){
--- Project-Course/src/app/recipes/recipes.model.ts
+++ Project-Course/src/app/recipes/recipes.model.ts
@@ -1,11 +1,16 @@
+import { Ingredient } from '../shared/ingredien.model';
+
 export class Recipe {
     public name: string;
     public description: string;
     public imagePath: string;
+    public ingridiens: Ingredient[];
+
     
-    constructor( name: string, desc: string, imagePath: string){
+    constructor( name: string, desc: string, imagePath: string, ingridens: Ingredient[]){
         this.name = name;
         this.description = desc;
         this.imagePath = imagePath;
+        this.ingridiens = ingridens;
     }
 }
