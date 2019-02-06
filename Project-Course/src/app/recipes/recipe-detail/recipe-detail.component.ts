5d42d0b3fc8667482a58f1a71e177d9c2a663b44
chaim1 <cohen1ch@gmail.com>
2019-02-06 15:03:48 +0200

    updat

--- Project-Course/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ Project-Course/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipes.model';
 import { RecipeService } from '../recipe.service';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -11,7 +11,7 @@ import { ActivatedRoute, Params } from '@angular/router';
 export class RecipeDetailComponent implements OnInit {
  recipe: Recipe;
  id: number;
-  constructor(private recipeService: RecipeService,private route: ActivatedRoute) { }
+  constructor(private recipeService: RecipeService,private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
     this.route.params.subscribe(
@@ -23,5 +23,9 @@ export class RecipeDetailComponent implements OnInit {
   }
   onAddToShoppingList(){
     this.recipeService.addIngredientToShoppingList(this.recipe.ingridiens);
+  }
+  onEditRecipe(){
+    this.router.navigate(['edit'], {relativeTo: this.route});
+    // this.router.navigate(['../', this.id, 'edit'], {relativeTo: this.route});
   }
 }
--- Project-Course/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Project-Course/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipes.model';
 import { RecipeService } from '../recipe.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-list',
@@ -9,9 +10,12 @@ import { RecipeService } from '../recipe.service';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[];
-  constructor(private recipeService: RecipeService) { }
+  constructor(private recipeService: RecipeService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.recipes = this.recipeService.getRecipes();
+  }
+  onNewRecipe(){
+    this.router.navigate(['new'], {relativeTo: this.route});
   }
 }
